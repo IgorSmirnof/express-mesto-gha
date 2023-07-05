e6a88facab8483b43ea96dc60e56e02f935c4aef
IGo <igor.smirna@gmail.com>
2023-07-05 15:23:36 +0300

    init: load files for work

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.send('hi')
