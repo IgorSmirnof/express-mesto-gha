d9702d05d533e7f2591fb69af9e7645d876f6cf5
IGo <igor.smirna@gmail.com>
2023-07-17 00:32:35 +0300

    test

--- routes/users/index.js
+++ routes/users/index.js
@@ -12,7 +12,7 @@ const URL_REXP = require('../../utils/rexp');
 router.get('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().parrern(URL_REXP),
+    link: Joi.string().required().pattern(URL_REXP),
   }),
 }), getUsers);
 
