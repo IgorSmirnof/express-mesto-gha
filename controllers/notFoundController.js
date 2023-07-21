ad31867ba165814accc0ee24cb1939a9d8afc435
IGo <igor.smirna@gmail.com>
2023-07-21 15:59:43 +0300

    refactor: после 2 ревью

--- controllers/notFoundController.js
+++ controllers/notFoundController.js
@@ -1,7 +1,7 @@
 // const { NOT_FIND_ERROR_CODE } = require('../utils/erroresConstans');
 const NotFoundError = require('../utils/errors/404-NotFound');
 
-const notFoundPage = () => new NotFoundError('Страница не найдена. 404');
+const notFoundPage = (req, res, next) => next(new NotFoundError('Страница не найдена. 404'));
 // (_req, res) => res
 // .status(NOT_FIND_ERROR_CODE)
 // .send({ message: 'Страница не найдена. 404' });
